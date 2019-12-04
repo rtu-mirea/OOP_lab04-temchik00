47e4fd95d3de82c431940cb6cbc2961d19037dfe
temchik00 <temchik00@yandex.ru>
2019-12-05 01:16:39 +0300

    made 1 & 3 ex.

---
+++ src/com/company/Main1.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        //Задание 1
+        try {
+            //упражнение 1
+            File MyFile1 = new File("MyFile1.txt");
+            MyFile1.createNewFile();
+            File MyFile2 = new File("C:\\DELETEME\\MyFile2.txt");
+            MyFile2.createNewFile();
+            File MyFile3 = new File("C:\\Java\\DELETEME\\MyFile3.txt");
+            MyFile3.createNewFile();
+            File folders = new File("First\\Second\\Third");
+            folders.mkdirs();
+
+            //Упражнение 2
+            System.out.println("Введите путь к файлу:");
+            Scanner in = new Scanner(System.in);
+            String way = in.next();
+            File t2 = new File(way);
+            System.out.println(t2.getName());
+            System.out.println(t2.getParent());
+
+            if (!way.contains(".")){
+                System.out.println("Введена папка а не файл");
+            }
+
+            File check = new File("MyFile1.txt");
+            if(check.exists()){
+                System.out.println("MyFile1.txt существует");
+            }else{
+                System.out.println("MyFile1.txt не существует");
+            }
+
+            System.out.println(t2.getAbsolutePath());
+
+            System.out.println(t2.length() + " байт");
+            if (t2.isDirectory()){
+                System.out.println("объект - папка");
+            }else{
+                System.out.println("объект - файл");
+            }
+
+            //Упражнение 3
+            File ndir = new File("newDirectory");
+            ndir.mkdir();
+            File folder = new File("C:\\Java\\Lab-4");
+            String[] listOfFiles_str = folder.list();
+            File[] listOfFiles = folder.listFiles();
+
+            System.out.println("содержимое папки приложения через list()");
+            for(String y: listOfFiles_str){
+                System.out.println(y);
+            }
+
+            System.out.println("содержимое папки приложения через listFiles()");
+            int fi = 0, dir = 0;
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    System.out.println(listOfFiles[i].getName());
+                    fi++;
+                } else if (listOfFiles[i].isDirectory()) {
+                    System.out.println(listOfFiles[i].getName());
+                    dir++;
+                }
+            }
+            System.out.println("Количество файлов: " + fi);
+            System.out.println("Количество папок: " + dir);
+
+            MyFile1.delete();
+            MyFile2.delete();
+            MyFile3.delete();
+            folders.delete();
+            folders = new File ("First\\Second");
+            folders.delete();
+            folders = new File ("First");
+            folders.delete();
+            ndir.delete();
+
+        }catch (Exception e){
+            System.out.println("error");
+        }
+    }
+}
---
+++ src/com/company/Main3.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class Main3 {
+    public static void num1 () throws IOException {
+        DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("T2.txt"));
+        DataInputStream dataIn = new DataInputStream(new FileInputStream("T1.txt"));
+        int i;
+        while ((i = dataIn.read()) != -1){
+            dataOut.write(i);
+        }
+        dataIn.close();
+        dataOut.close();
+    }
+
+    public static void num2() throws Exception{
+        BufferedReader inb = new BufferedReader(new FileReader("A.txt"), 128);
+        BufferedWriter outb = new BufferedWriter(new FileWriter("B.txt"));
+        char[] buf = new char[128];
+
+        while ((inb.read(buf)) != -1){
+            outb.write(buf);
+            outb.newLine();
+        }
+        outb.flush();
+        outb.close();
+        inb.close();
+    }
+
+    public static void num3() throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("A.txt"), "UTF-8"));
+        System.out.println(Charset.defaultCharset().name());
+        String s = "";
+        while ((s = in.readLine()) != null){
+            System.out.println(s);
+        }
+        System.out.println();
+        in = new BufferedReader(new InputStreamReader(new FileInputStream("B.txt"), "UTF-16"));
+        while ((s = in.readLine()) != null){
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        //Задание 2
+        try {
+            num1();
+            num2();
+            num3();
+        } catch (Exception e) {
+            System.out.println("Ошибка");
+        }
+    }
+}
